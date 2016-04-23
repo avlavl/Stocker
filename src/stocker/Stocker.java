322576f61576be0d0c8fed43493f4af5036f9c41
avlavl <dawei_cool@126.com>
2016-04-23 19:49:30 +0800

    Add Stocker base support

---
+++ src/stocker/Stocker.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stocker;
+
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author zhangxr
+ */
+public class Stocker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
+         */
+        try {
+            /* Set L&F to Metal at first. This is the default L&F */
+            // Set cross-platform Java L&F (also called "Metal")
+            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+
+            /* If it is Windows platform, use native L&F */
+            javax.swing.UIManager.LookAndFeelInfo[] installedLookAndFeels = javax.swing.UIManager.getInstalledLookAndFeels();
+            for (UIManager.LookAndFeelInfo installedLookAndFeel : installedLookAndFeels) {
+                if ("Windows".equals(installedLookAndFeel.getName())) {
+                    UIManager.setLookAndFeel(installedLookAndFeel.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        }
+        //</editor-fold>
+
+        StockerView sv = new StockerView();
+        sv.setVisible(true);
+    }
+
+}
